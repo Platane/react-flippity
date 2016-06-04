c73635f87ebfa9008bf63d085f8147446c119d71
Platane <brongniart.arthur@gmail.com>
2016-06-04 23:31:23 +0200

    fix: wait for width and height animation end

--- src/computeMotion.js
+++ src/computeMotion.js
@@ -5,5 +5,9 @@ export const acc = ( k, b, x, v, target ) =>
 export const cold = ( precision, p, target, v ) =>
        Math.abs( v.x ) < precision
     && Math.abs( v.y ) < precision
+    && Math.abs( v.sx ) < precision
+    && Math.abs( v.sy ) < precision
     && Math.abs( p.x - target.x ) < precision
     && Math.abs( p.y - target.y ) < precision
+    && Math.abs( p.sx - target.sx ) < precision
+    && Math.abs( p.sy - target.sy ) < precision
